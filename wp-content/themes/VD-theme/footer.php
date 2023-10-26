0354cbc40327b34b47b5627e670db810912c5b7e
Blin Kusari <blin.kusari@atis.al>
2023-10-26 13:26:02 +0200

    footer changes

--- wp-content/themes/VD-theme/footer.php
+++ wp-content/themes/VD-theme/footer.php
@@ -11,15 +11,16 @@ $footer_menu_label_4 = get_field('footer_menu_label_4', 'options');
 <footer class="footer">
     <div class="container">
         <div class="row footer-container">
-            
+
             <div class="col-md-6 left-part">
-            <img class="logo" src="<?= get_theme_file_uri() ?>/images/impact-rec-logo.svg" alt="">
+                <img class="logo" src="<?= get_theme_file_uri() ?>/images/logo-footer.svg" alt="">
             </div>
             <div class="col-md-6 right-part">
-                <p style="margin-bottom:0">DESA AUTOGLASS AG | Thunstrasse 162 | 3074 Muri b. Bern </p>
+                <p style="margin-bottom:0">DESA AUTOGLASS AG |<br style="display:none"> Thunstrasse 162 |<br style="display:none"> 3074 Muri b. Bern </p>
                 <div class="d-flex">
-                <a href="mailto:reservation@desa-autoglass.ch">reservation@desa-autoglass.ch</a>
-                <p class="helpline">| Helpline <a href="tel: 0848804022">0848 80 40 22</a></p>
+                    <a style="margin-right:5px"
+                        href="mailto:reservation@desa-autoglass.ch">reservation@desa-autoglass.ch </a>
+                    <p class="helpline"> | Helpline <a href="tel: 0848804022">0848 80 40 22</a></p>
                 </div>
                 <div class="language">
                     <a class="active" href="">DE</a> <span>|</span> <a href="">FR</a> <span>|</span><a href="">IT</a>
@@ -28,5 +29,20 @@ $footer_menu_label_4 = get_field('footer_menu_label_4', 'options');
         </div>
     </div>
 </footer>
+<div class="after-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 left">
+                <div class="links"><a href="https://reservation.desa-autoglass.ch/" class="pr-4">Datenschutzerklärung</a></div>
+            </div>
+            <div class="col-md-6 right">
+                <a href="https://impact-digital.ch/" target="_blank">
+                    Webdesign by Impact Digital Gmbh
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 <?php wp_footer(); ?>
+
 </html>
