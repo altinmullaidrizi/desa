ed1ab121ea3e4ca39b635bf024de2be097ab2f70
Blin Kusari <blin.kusari@atis.al>
2023-11-01 16:57:51 +0100

    google script added

--- wp-content/themes/VD-theme/header.php
+++ wp-content/themes/VD-theme/header.php
@@ -13,7 +13,13 @@ if (!$has_page_banner) {
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="robots" content="index,follow" />
-
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WSRWFW2');</script>
+    <!-- End Google Tag Manager -->
     <title>
         <?php
         global $page, $paged;
@@ -40,7 +46,10 @@ if (!$has_page_banner) {
 </head>
 
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSRWFW2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     <header class="header">
         <div class="container">
